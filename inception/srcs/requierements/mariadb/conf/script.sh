48a7852443247f4c71d89f2a42e2114eb814cafd
kcharbon <kcharbon@Inception42.kcharbon.42.fr>
2025-08-25 17:25:44 +0200

    variable env pour mariadb init

--- inception/srcs/requierements/mariadb/conf/script.sh
+++ inception/srcs/requierements/mariadb/conf/script.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-mysql_install_db
+sed -i "s/\${MARIADB_USER}/$MARIADB_USER/g" /etc/mysql/init.sql
+sed -i "s/\${MARIADB_PASSWORD}/$DB_PASSWORD/g" /etc/mysql/init.sql
+sed -i "s/\${MARIADB_DATABASE}/$MARIADB_DATABASE/g" /etc/mysql/init.sql
+sed -i "s/\${MARIADB_USER2}/$MARIADB_USER2/g" /etc/mysql/init.sql
+sed -i "s/\${MARIADB_PASSWORD2}/$DB_PASSWORD2/g" /etc/mysql/init.sql
+
+mariadb-install-db
 mysqld
 
