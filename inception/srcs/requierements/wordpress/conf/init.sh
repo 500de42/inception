097787676ed5b47d13b9f6025ebdc23cf7070e2c
kcharbon <kalvinsam31@gmail.com>
2025-08-29 17:58:08 +0200

    wordpress mis en place, pb database

---
+++ inception/srcs/requierements/wordpress/conf/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+WP_DIR="/var/www/html"
+WP_CONFIG="$WP_DIR/wp-config.php"
+
+# Vérifier si wp-config.php existe déjà
+if [ ! -f "$WP_CONFIG" ]; then
+    cp "$WP_DIR/wp-config-sample.php" "$WP_CONFIG"
+
+    # Remplacer les valeurs de base de données avec les variables d'environnement
+    sed -i "s/database_name_here/${DB_NAME}/" "$WP_CONFIG"
+    sed -i "s/username_here/${DB_USER}/" "$WP_CONFIG"
+    sed -i "s/password_here/${DB_PASSWORD}/" "$WP_CONFIG"
+    sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG"
+
+    echo "wp-config.php généré automatiquement."
+else
+    echo "wp-config.php existe déjà, aucun changement effectué."
+fi
+
+php-fpm82 -F
---
+++ inception/srcs/requierements/wordpress/conf/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_NAME', 'nom_de_ta_base');      // Nom de la base
+define('DB_USER', 'utilisateur');         // Nom d'utilisateur MariaDB
+define('DB_PASSWORD', 'mot_de_passe');    // Mot de passe de l'utilisateur
+define('DB_HOST', 'localhost');           // Serveur MariaDB (ou nom du service Docker)
+
