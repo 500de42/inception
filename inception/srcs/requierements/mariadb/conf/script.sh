bddc0be342ad3ae192ccc20d21b1d3a36197d4fb
kcharbon <kalvinsam31@gmail.com>
2025-09-05 17:21:22 +0200

    mise en place du service healthcheck

--- inception/srcs/requierements/mariadb/conf/script.sh
+++ inception/srcs/requierements/mariadb/conf/script.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+chown -R mysql:mysql /var/lib/mysql /etc/mysql/init.sql
+chmod 750 /var/lib/mysql /etc/mysql/init.sql
+
 sed -i "s/\${MARIADB_USER}/$MARIADB_USER/g" /etc/mysql/init.sql
-sed -i "s/\${MARIADB_PASSWORD}/$DB_PASSWORD/g" /etc/mysql/init.sql
+sed -i "s/\${DB_PASSWORD}/$DB_PASSWORD/g" /etc/mysql/init.sql
 sed -i "s/\${MARIADB_DATABASE}/$MARIADB_DATABASE/g" /etc/mysql/init.sql
 sed -i "s/\${MARIADB_USER2}/$MARIADB_USER2/g" /etc/mysql/init.sql
-sed -i "s/\${MARIADB_PASSWORD2}/$DB_PASSWORD2/g" /etc/mysql/init.sql
+sed -i "s/\${DB_PASSWORD2}/$DB_PASSWORD2/g" /etc/mysql/init.sql
+sed -i "s/\${MARIADB_ROOT_PASSWORD}/$MARIADB_ROOT_PASSWORD/g" /etc/mysql/init.sql
 
-mariadb-install-db
-mysqld
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initialisation de la base..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
 
+mysqld --user=mysql
