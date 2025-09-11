4b8f1a0ebfc5de68da030774d7312d620ba0f40c
kalvin <kalvinsam31@gmail.com>
2025-09-11 19:53:39 +0200

    mise en place de Redis

--- inception/srcs/requierements/wordpress/conf/init.sh
+++ inception/srcs/requierements/wordpress/conf/init.sh
@@ -46,43 +46,25 @@ chown -R www-data:www-data /var/www/html
 
 php-fpm82 -F
 
+# set -e
 
+# WP_DIR="/var/www/html"
+# WP_CONFIG="$WP_DIR/wp-config.php"
+# chown -R www-data:www-data "$WP_PATH" || true
+# # Vérifier si wp-config.php existe déjà
+# if [ ! -f "$WP_CONFIG" ]; then
+#     cp "$WP_DIR/wp-config-sample.php" "$WP_CONFIG"
 
+#     # Remplacer les valeurs de base de données avec les variables d'environnement
+#     sed -i "s/database_name_here/${DB_NAME}/" "$WP_CONFIG"
+#     sed -i "s/username_here/${DB_USER}/" "$WP_CONFIG"
+#     sed -i "s/password_here/${DB_PASSWORD}/" "$WP_CONFIG"
+#     sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG"
 
+#     echo "wp-config.php généré automatiquement."
+# else
+#     echo "wp-config.php existe déjà, aucun changement effectué."
+# fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-set -e
-
-WP_DIR="/var/www/html"
-WP_CONFIG="$WP_DIR/wp-config.php"
-chown -R www-data:www-data "$WP_PATH" || true
-# Vérifier si wp-config.php existe déjà
-if [ ! -f "$WP_CONFIG" ]; then
-    cp "$WP_DIR/wp-config-sample.php" "$WP_CONFIG"
-
-    # Remplacer les valeurs de base de données avec les variables d'environnement
-    sed -i "s/database_name_here/${DB_NAME}/" "$WP_CONFIG"
-    sed -i "s/username_here/${DB_USER}/" "$WP_CONFIG"
-    sed -i "s/password_here/${DB_PASSWORD}/" "$WP_CONFIG"
-    sed -i "s/localhost/${DB_HOST}/" "$WP_CONFIG"
-
-    echo "wp-config.php généré automatiquement."
-else
-    echo "wp-config.php existe déjà, aucun changement effectué."
-fi
-
-cat wp-config.php
-php-fpm82 -F
+# cat wp-config.php
+# php-fpm82 -F
